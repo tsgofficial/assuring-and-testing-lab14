6c26f20d132847ad7591db44a4fb2e554103efdc
tsgofficial <baihguibno@gmail.com>
2025-12-08 22:55:31 +0800

    ci test

--- src/test/java/lab14/DivisionTest.java
+++ src/test/java/lab14/DivisionTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DivisionTest {
 
+    /// ci test
     @Test
     void testDivideNormal() {
         Division calc = new Division();
